3899f432e9229bf83fd6769277741ad1177a5e52
Terris Kremer <terris.kremer@gmail.com>
2019-10-22 12:01:27 -0400

    re add new project button

--- src/components/projects/ProjectsMenu.js
+++ src/components/projects/ProjectsMenu.js
@@ -2,16 +2,17 @@ import React from 'react';
 import { Menu } from 'semantic-ui-react';
 import { withRouter } from 'react-router-dom';
 import { routes } from '../../constants';
+import NewProjectModal from './NewProjectModal';
 
-const ProjectsMenu = ({ projects, history, location }) => {
+const ProjectsMenu = ({ projects, history, location, authUser }) => {
   const activeItem = location.pathname.split('/')[2];
   return (
     <Menu pointing secondary vertical fluid>
-      <Menu.Item header as="a" active={!activeItem}
-        onClick={() => history.push(routes.PROJECTS)}>Projects</Menu.Item>
+      <Menu.Item header as="a" active={!activeItem} onClick={() => history.push(routes.PROJECTS)}>Projects</Menu.Item>
       {!!projects.length && projects.map(project => {
         return <Menu.Item key={project.id} as="a" active={activeItem === project.id} onClick={() => history.push(`${routes.PROJECTS}/${project.id}`)}>{project.name}</Menu.Item>
       })}
+      <Menu.Item><NewProjectModal authUser /></Menu.Item>
     </Menu>
   )
 }
